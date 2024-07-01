9a7386184aad7b02fbd32a572044deec98554dd9
Miguel Pegna <miguelpegna@outlook.com>
2024-06-30 22:48:14 -0600

    core/Controller update

--- core/Controller.php
+++ core/Controller.php
@@ -4,29 +4,24 @@
     class Controller{
 
         public $info;
+        public $scripts;
         
         public function __construct(){
         }
 
         /**
         * Description: method for obtain view of controller 
-        * @param object $controller -> name of controller 
-        * @param string $view -> name of view
+        * @param string $view -> view path in views directory
         */
-        public function view($controller, $view){
-            $controller = get_class($controller);   //path Controller  TODO: App\Controllers\NameController
-            $cleanPathController = str_replace('app\Controllers\\', '', $controller);  //Remove the path and just keep the name of controller   TODO: NameController
-            $viewController = str_replace('Controller', '', $cleanPathController);     //Remove the word Controller and just Keep the simple name of controller  TODO Name
-            
+        public function view($view){
             $view = str_replace('.', '/', $view);
-            $pathFile='../resources/views/'.$viewController. '/'. $view. '.php';
-            if(file_exists($pathFile)){
+            $pathView='../resources/views/'.$view. '.php';
+            if(file_exists($pathView)){
                 ob_start();
                 # Converts the received arguments into variables.
-                if ($this->info !== null) {
-                    extract($this->info);
-                }
-                include "$pathFile";
+                if ($this->info !== null) extract($this->info);
+                if ($this->scripts !== null) extract($this->scripts);
+                include "$pathView";
                 $view = ob_get_clean();
                 echo $view;
             }
@@ -40,29 +35,31 @@
         * @param string $key -> name of variable 
         * @param string $value -> value of variable 
         */
-        public function setVar($key, $value){
+        public function setVar($key='', $value=''){
             $this->info[$key] = $value;
         }
 
         /**
         * Description: method for send scripts JS or CCS sheets
-        * @param string $ext -> cjs = JS in commonJS  || mjs = JS in EsModule || css = css file
+        * @param string $key -> name of variable for script
+        * @param string $type -> commonJS || EsModules || css = css file
         * @param string $name -> JS file name or css file name 
         */
-        public function getScript($ext, $name){
-            if($ext != 'cjs' || $ext != 'mjs' || $ext != 'css'){
+        public function getScript($type='', $scriptName=''){
+            $scriptName = str_replace('.', '/', $scriptName);
+            if($type != 'commonjS' || $type != 'EsModules' || $type != 'css'){
                 $file = '';
             }
-            if($ext == 'cjs' ){  //script in commonJS
-                $file = '<script src="'. JS_FILES .$name. '.js"></script>';
+            if($type == 'commonJS' ){  //script in commonJS
+                $file = '<script src="'. JS_FILES .'/'. $scriptName. '.js"></script>';
             }
-            if($ext == 'mjs' ){
-                $file = '<script type="module" src="'. JS_FILES .$name. '.js"></script>';
+            if($type == 'EsModules' ){ //script in EsModule
+                $file = '<script type="module" src="'. JS_FILES .'/'. $scriptName. '.js"></script>';
             }
-            if($ext == 'css' ){    //script in EsModule
-                $file = '<link href="'. CSS_FILES .$name. '.css" rel="stylesheet">';
+            if($type == 'css' ){    
+                $file = '<link href="'. CSS_FILES .'/'. $scriptName. '.css" rel="stylesheet">';
             }
-            return $file;
+            return $this->scripts['scripts'] = $file;
         }
 
     }
